3556369ce43e6ab94d9f6a5e543fc8853b739d48
omwitsa-etole <omwitsaetole@gmail.com>
2024-04-25 08:29:52 +0300

    add admin

--- property_management_3.jetadmin.io (1)/july.jetadmin.io/src/loader-surveys.ts
+++
@@ -1,9 +0,0 @@
-import { generateSurveys } from './extensions/surveys'
-
-import { window } from './utils/globals'
-
-if (window) {
-    ;(window as any).extendPostHogWithSurveys = generateSurveys
-}
-
-export default generateSurveys
